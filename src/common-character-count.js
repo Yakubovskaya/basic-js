4132511e8bfca9b5ced37094c2849997ea316e36
Marina Yakubovskaya <maribars93@yandex.ru>
2025-11-11 00:36:36 +0300

    feat: implement common-character-count, encode-line, hanoi-tower, transform-array functions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,9 +12,22 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const count = {};
+  let result = 0;
+
+  for (const char of s1) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  for (const char of s2) {
+    if (count[char] > 0) {
+      result++;
+      count[char]--;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given a string, return its encoding version.
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  let result = "";
+  let i = 0;
+  while (i < str.length) {
+    let count = 1;
+    while (str[i] === str[i + count]) {
+      count++;
+    }
+    result += (count > 1 ? count : "") + str[i];
+    i += count;
+  }
+  return result;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
